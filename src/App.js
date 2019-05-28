1782b9a63249bb34adf6b46c03546a118958243c
dajmmos007 <dajmos007@gmail.com>
2019-05-28 10:29:56 +0200

    adding first steps

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import Layout from "../src/Components/Layout/Layout"
+import BurgerBuilder from '../src/Components/BurgerBuilder/BurgerBuilder'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src="" className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Layout></Layout>
+        <BurgerBuilder></BurgerBuilder>
       </div>
     );
   }
